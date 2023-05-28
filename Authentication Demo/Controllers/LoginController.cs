2d6c57eb2ee488b560b3aad99828fe26191bfe0e
Rayyan-Asia <RayyanTawfieg@gmail.com>
2023-05-28 08:39:31 +0300

    Create initial demo controller

---
+++ Authentication Demo/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Authentication_Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+        [HttpPost("authenticate")]
+        public ActionResult<String> Index(AuthenticationRequestBody authenticationRequestBody)
+        {
+            return View();
+        }
+    }
+
+    public class AuthenticationRequestBody
+    {
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+    }
+}
