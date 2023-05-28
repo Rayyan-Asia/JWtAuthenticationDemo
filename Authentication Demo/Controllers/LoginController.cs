bfea1e918abbd9c934b8b1e1908a9bd3c8071d8a
Rayyan-Asia <RayyanTawfieg@Gmail.com>
2023-05-28 11:40:15 +0300

    Completed Demo

--- Authentication Demo/Controllers/LoginController.cs
+++ Authentication Demo/Controllers/LoginController.cs
@@ -1,15 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Authentication_Demo.Controllers
 {
+    
+    
     [Route("api/[controller]")]
     [ApiController]
     public class LoginController : Controller
     {
-        [HttpPost("authenticate")]
-        public ActionResult<String> Index(AuthenticationRequestBody authenticationRequestBody)
+        private readonly IConfiguration _configuration;
+
+        public LoginController(IConfiguration configuration)
         {
-            return View();
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+        [HttpPost("authenticate")]
+        public ActionResult<String> Authenticate(AuthenticationRequestBody authenticationRequestBody)
+        {
+            var user = ValidateUserCredentials(authenticationRequestBody.UserName, authenticationRequestBody.Password);
+            if (user == null) return Unauthorized();
+
+            var securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_configuration["Authentication:SecretForKey"]));
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+            {
+                new Claim("user_name",authenticationRequestBody.UserName),
+                new Claim("password", authenticationRequestBody.Password),
+                new Claim("role","Admin")
+            };
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                _configuration["Authentication:Issuer"],
+                _configuration["Authentication:Audience"],
+                claims,
+                DateTime.UtcNow,
+                DateTime.UtcNow.AddHours(1),
+                signingCredentials);
+
+            var token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+            return Ok(token);
+
+        }
+
+        private object? ValidateUserCredentials(string? userName, string? password)
+        {
+            if (userName == "Rayyan" && password == "Rayyan123")
+                return new object();
+            return null;
         }
     }
 
@@ -18,4 +60,6 @@ namespace Authentication_Demo.Controllers
         public string? UserName { get; set; }
         public string? Password { get; set; }
     }
+
+    
 }
--- Authentication Demo/Program.cs
+++ Authentication Demo/Program.cs
@@ -1,3 +1,6 @@
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer(options=>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Authentication:Issuer"],
+            ValidAudience = builder.Configuration["Authentication:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.ASCII.GetBytes(builder.Configuration["Authentication:SecretForKey"]))
+        };
+    });
 
 var app = builder.Build();
 
@@ -18,6 +35,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
